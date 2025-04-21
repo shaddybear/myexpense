d689a6c349f2b88a2ebaeac086b14611e9766194
ShaddyBear🐻 <gauravpaul7887@gmail.com>
2025-04-21 10:18:31 +0530

    [Feature]:add new -layout.tsx file here

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { Stack } from 'expo-router';
+
+const index = () => {
+  return <Stack> </Stack>;
+  
+};
+
+export default index
+
+const styles = StyleSheet.create({})
