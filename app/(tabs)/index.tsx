122bee9feb8abfa8c6a6286670551dff5eb8befd
ShaddyBear🐻 <gauravpaul7887@gmail.com>
2025-04-21 11:10:34 +0530

    [Feature]: new welcome picture add

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,22 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { colors } from '@/constants/theme'
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { colors } from "@/constants/theme";
 
 const index = () => {
   return (
     <View style={styles.container}>
-      <Text>index</Text>
+      <Image
+        style={styles.logo}
+        resizeMode="contain"
+        // source={require('../assets/images/welcome1.jpg')}
+        // source={require('../../../../assets/images/welcome1.jpg')}
+        source={require('../../assets/images/welcome2.jpg')}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default index
+export default index;
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: colors.neutral900
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colors.neutral900,
+  },
+ logo: {
+    height: "20%",
+    aspectRatio: 1,
+ }
+});
