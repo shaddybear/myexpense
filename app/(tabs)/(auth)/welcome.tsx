463396487c3af73be48be4f6cd7660cea990fd3d
ShaddyBear🐻 <124757807+shaddybear@users.noreply.github.com>
2025-08-02 00:05:30 +0530

    fix: lots unused code! i remove

--- app/(tabs)/(auth)/welcome.tsx
+++ app/(tabs)/(auth)/welcome.tsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import ScreenWrapper from '@/components/ScreenWrapper'
-import Typo from '@/constants/Typo'
+import Typo from '@/components/Typo'
 import { colors, spacingX, spacingY } from '@/constants/theme'
 import { verticalScale } from '@/utils/styling'
 
 const index = () => {
   return (
     <ScreenWrapper>
-     <Typo size={40} fontWeight={700} color='green'> welcome page </Typo>
+     {/* <Typo size={40} fontWeight={700} color='green'> welcome page </Typo>
+     <View style={styles.container}>
+  {/* login button & image */}
+  <View>
+    <TouchableOpacity style={styles.loginButton}>
+      <Typo fontWeight={"500"}>Sign in</Typo>
+    </TouchableOpacity>
+
+    <Image
+      source={require("../assets/images/welcome2.png")} // Adjust the path as necessary
+      style={styles.welcomeImage}
+      resizeMode="contain"
+    />
+<View style={styles.footer}>
+    
+</View>
+
+  </View>
+
     </ScreenWrapper>
   )
 }
